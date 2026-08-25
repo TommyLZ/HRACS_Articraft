faa939b99643729f7efbc32e099d056cee605a05
TommyLiuZ <5451679+tommyliuz@user.noreply.gitee.com>
2026-08-25 08:34:09 +0800

    Initial release of the HRACS experimental artifact

--- Operation_UnitTest/Operation_Test_Unit.cpp
+++ Operation_UnitTest/Operation_Test_Unit.cpp
@@ -10,19 +10,12 @@ int main()
 {
     const int TEST_NUM = 1000;
 
-                                                                 
-                                          
-                                
-                                                                 
     pbc_param_t param;
     pairing_t pairing;
 
     pbc_param_init_a_gen(param, 160, 1024);
     pairing_init_pbc_param(pairing, param);
 
-                                                                 
-                          
-                                                                 
     element_t g, h, gt, x;
 
     element_init_G1(g, pairing);
@@ -34,9 +27,6 @@ int main()
     element_random(h);
     element_random(x);
 
-                                                                 
-                              
-                                                                 
     element_t hash_elem;
     element_init_G1(hash_elem, pairing);
 
@@ -50,25 +40,19 @@ int main()
             msg,
             sizeof(msg),
             "Hello PBC Benchmark %d",
-            i
-        );
+            i);
 
         element_from_hash(
             hash_elem,
-            static_cast<void*>(msg),
-            len
-        );
+            static_cast<void *>(msg),
+            len);
     }
 
     auto hash_end = steady_clock::now();
 
     double hash_avg_ns =
-        duration_cast<nanoseconds>(hash_end - hash_start).count()
-        / static_cast<double>(TEST_NUM);
+        duration_cast<nanoseconds>(hash_end - hash_start).count() / static_cast<double>(TEST_NUM);
 
-                                                                 
-                                     
-                                                                 
     element_t exp_result;
     element_init_G1(exp_result, pairing);
 
@@ -82,12 +66,8 @@ int main()
     auto exp_end = steady_clock::now();
 
     double exp_avg_ns =
-        duration_cast<nanoseconds>(exp_end - exp_start).count()
-        / static_cast<double>(TEST_NUM);
+        duration_cast<nanoseconds>(exp_end - exp_start).count() / static_cast<double>(TEST_NUM);
 
-                                                                 
-                           
-                                                                 
     auto pair_start = steady_clock::now();
 
     for (int i = 0; i < TEST_NUM; ++i)
@@ -98,12 +78,8 @@ int main()
     auto pair_end = steady_clock::now();
 
     double pair_avg_ns =
-        duration_cast<nanoseconds>(pair_end - pair_start).count()
-        / static_cast<double>(TEST_NUM);
+        duration_cast<nanoseconds>(pair_end - pair_start).count() / static_cast<double>(TEST_NUM);
 
-                                                                 
-             
-                                                                 
     cout << "======================================" << endl;
     cout << "Benchmark Results (" << TEST_NUM << " runs)" << endl;
     cout << "Type A parameters: r = 160 bit, q = 1024 bit" << endl;
@@ -124,9 +100,6 @@ int main()
          << " (" << pair_avg_ns / 1e3 << " us)"
          << endl;
 
-                                                                 
-            
-                                                                 
     element_clear(g);
     element_clear(h);
     element_clear(gt);
